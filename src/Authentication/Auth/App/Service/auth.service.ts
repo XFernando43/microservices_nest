244125c7996b7e0c080f4c4dc74c07a7a9bb6c46
XFernando43 <U20171E997@upc.edu.pe>
2024-01-03 19:30:37 -0500

    authorization service add.

--- src/Authentication/Auth/App/Service/auth.service.ts
+++ src/Authentication/Auth/App/Service/auth.service.ts
@@ -1,17 +1,35 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { CreateAuthDto } from '../../Domain/dto/create-auth.dto';
 import { UpdateAuthDto } from '../../Domain/dto/update-auth.dto';
 import { UsersService } from 'src/Authentication/Users/App/Service/userService';
+import { JwtService } from '@nestjs/jwt';
+import { CreateUserDto } from 'src/Authentication/Users/Domain/dto/create-user.dto';
 
 @Injectable()
 export class AuthService {
-  constructor(private readonly userService: UsersService){
-
-  }
+  constructor(private readonly userService: UsersService,
+              private readonly jwtService:JwtService){}
 
   async validateUser(username:string, password:string):Promise<any>{
     const user = await this.userService.findByUsername(username,password);
     const isValidPassword = await this.userService.checkPassword(password,user.account.password); 
-    return null;
+    if(user && isValidPassword){
+      return user;
+    }
+    throw new HttpException("Not User Validated", HttpStatus.INTERNAL_SERVER_ERROR);
   }
+
+  async signIn(user:any){
+    const payload = {
+      username:user.username,
+      sub:user.id,
+    }
+
+    return {access_token: this.jwtService.sign(payload)};
+  }
+
+  async signUp(userDto: CreateUserDto){
+    return await this.userService.createUser(userDto);
+  }
+
 }
